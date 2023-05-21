951f143ec63afbe63805d367529c22e1ed8f038e
aigeoo <yosifsalimalessaei@gmail.com>
2023-05-21 14:35:56 +0400

    feat: update installation configurations

--- install.sh
+++ install.sh
@@ -1,25 +1,11 @@
-#!/bin/bash
+go install -v github.com/owasp-amass/amass/v3/...@master
 
-# Install required tools
-echo "[+] Installing subfinder"
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 
-echo "[+] Installing assetfinder"
 go install github.com/tomnomnom/assetfinder@latest
 
-echo "[+] Installing sublist3r"
-git clone https://github.com/aboul3la/Sublist3r.git
-cd Sublist3r && pip install -r requirements.txt
-
-echo "[+] Installing knockpy"
-git clone https://github.com/guelfoweb/knock.git
-cd knock && sudo python setup.py install
-
-echo "[+] Installing massdns"
-git clone https://github.com/blechschmidt/massdns.git
-cd massdns && make && cp bin/massdns /usr/bin/
-
-echo "[+] Installing httpx"
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 
-echo "[+] All tools installed successfully!"
+cd /vendor && git clone https://github.com/aboul3la/Sublist3r.git & cd Sublist3r && pip install -r requirements.txt
+
+cd /vendor && git clone https://github.com/guelfoweb/knock.git && cd knock && python setup.py install
