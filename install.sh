e96ac19ef627efcc90aadd5a7ed1473e4e5950b5
aigeoo <yosifsalimalessaei@gmail.com>
2023-05-21 16:34:32 +0400

    fix: update install.sh file

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ go install github.com/tomnomnom/assetfinder@latest
 
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 
-cd /vendor && git clone https://github.com/aboul3la/Sublist3r.git & cd Sublist3r && pip install -r requirements.txt
+cd vendor/ && git clone https://github.com/aboul3la/Sublist3r.git & cd Sublist3r && pip install -r requirements.txt
 
-cd /vendor && git clone https://github.com/guelfoweb/knock.git && cd knock && python setup.py install
+cd vendor/ && git clone https://github.com/guelfoweb/knock.git && cd knock && python setup.py install
